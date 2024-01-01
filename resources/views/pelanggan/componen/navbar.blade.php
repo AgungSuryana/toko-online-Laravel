143c8585dffc22332643dcc5a17c94668c142c85
DavinArisandi <agungsur84@gmail.com>
2024-01-01 07:20:37 +0700

    navbar

---
+++ resources/views/pelanggan/componen/navbar.blade.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-dark navbar-expand-lg" style="background-color: #164863; font-weight: bold;">
+    <div class=" container">
+        <a class="navbar-brand fs-5" href="#">LaZaDi</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end gap-4" id="navbarSupportedContent">
+            <ul class="navbar-nav gap-4 ">
+                <li class="nav-item">
+                    <a class="nav-link fs-5 active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link fs-5 " href="#">Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fs-5" href="#">Contact us</a>
+                </li>
+
+            </ul>
+            <div class="d-flex gap-4 align-items-center">
+                <div>
+                    <a href="#" class="fs-5 ">
+                        <i class=" fa-solid icon-nav fa-bag-shopping"></i>
+                    </a>
+                    <div class="circle">10</div>
+                </div>
+                <button class="btn btn-success" type="button">login | Register</button>
+            </div>
+        </div>
+    </div>
+</nav>
